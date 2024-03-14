57de092e5b1ee4ab4e2677ca5c4f49bfe7e4606f
laszloekovacs <laszloekovacs@gmail.com>
2024-03-14 11:12:48 +0100

    added env

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from '@remix-run/node'
+import { json, type MetaFunction } from '@remix-run/node'
+import { Link, useLoaderData } from '@remix-run/react'
+import { db } from '~/services.server'
 
 export const meta: MetaFunction = () => {
 	return [
@@ -7,10 +9,30 @@ export const meta: MetaFunction = () => {
 	]
 }
 
+export const loader = async () => {
+	const topics = await db.cards.distinct('topic')
+	return json({ topics })
+}
+
 export default function Index() {
+	const { topics } = useLoaderData<typeof loader>()
+
 	return (
 		<div>
 			<h1>Welcome to Flashcards!</h1>
+
+			<h2>Pick a topic to practice</h2>
+			<ul>
+				{topics.map(topic => (
+					<TopicListItem key={topic} topic={topic} />
+				))}
+			</ul>
 		</div>
 	)
 }
+
+export const TopicListItem = ({ topic }: { topic: string }) => (
+	<li>
+		<Link to={`/flashcards/${topic}`}>{topic}</Link>
+	</li>
+)

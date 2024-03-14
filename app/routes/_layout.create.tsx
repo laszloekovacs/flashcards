9a078a529e23801d3ef79123c23c1b88776f47a4
laszloekovacs <laszloekovacs@gmail.com>
2024-03-14 15:25:31 +0100

    Refactor create note functionality

--- app/routes/_layout.create.tsx
+++ app/routes/_layout.create.tsx
@@ -1,10 +1,36 @@
-import { Form, useFetcher } from '@remix-run/react'
+import { ActionFunctionArgs } from '@remix-run/node'
+import { useFetcher } from '@remix-run/react'
+import { WithId } from 'mongodb'
+import invariant from 'tiny-invariant'
+import { db } from '~/services.server'
+
+export const action = async ({ request }: ActionFunctionArgs) => {
+	const formData = await request.formData()
+
+	const topic = formData.get('topic') as string
+	const title = formData.get('title') as string
+	const notes = formData.get('notes') as string
+
+	invariant(topic, 'topic is required')
+	invariant(notes, 'notes is required')
+
+	// insert into database
+	const insertionResult = await db.cards.insertOne({
+		topic,
+		title,
+		notes
+	} as WithId<FlashCard>)
+
+	invariant(insertionResult.acknowledged, 'failed to create')
+
+	return new Response('created', { status: 201 })
+}
 
 export default function CreateNote() {
 	const fetcher = useFetcher()
 
 	return (
-		<fetcher.Form method='POST' action='/api'>
+		<fetcher.Form method='POST'>
 			<fieldset disabled={fetcher.state != 'idle'}>
 				<div className='input-group mb-4'>
 					<label htmlFor='topic' className='input-group-text'>
